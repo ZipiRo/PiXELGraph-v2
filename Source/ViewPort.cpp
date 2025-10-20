b43f5feceb3cddbceba1148e51da2f514098a049
ZipiRo <ionutperdun777@gmail.com>
2025-10-20 21:45:56 +0300

    Refactor View class by reorganizing SetPosition and SetScreenCenter methods for improved readability

--- Source/ViewPort.cpp
+++ Source/ViewPort.cpp
@@ -1,16 +1,6 @@
 #include "Vector2.h"
 #include "Core/ViewPort.h"
 
-void View::SetPosition(const Vector2 &newPosition) 
-{
-    position = newPosition;
-}
-
-void View::SetScreenCenter(const Vector2 &center)
-{
-    screenCenter = center;
-}
-
 View::View()
 {
     this->position = Vector2::ZERO;
@@ -32,7 +22,7 @@ View::View(float positionX, float positionY, float screeCenterX, float screeCent
     zoom = 1;
 }
 
-void View::Move(const Vector2& delta)
+void View::Move(const Vector2 &delta)
 {
     position += delta;
 }
@@ -45,15 +35,26 @@ void View::SetZoom(float newZoom)
 void View::Zoom(float amount)
 {
     zoom += amount;
-    if (zoom < 0.1f) zoom = 0.1f;
+    if (zoom < 0.1f)
+        zoom = 0.1f;
 }
 
-Vector2 View::WorldToScreen(const Vector2& worldPosition)
+void View::SetPosition(const Vector2 &newPosition)
+{
+    position = newPosition;
+}
+
+void View::SetScreenCenter(const Vector2 &center)
+{
+    screenCenter = center;
+}
+
+Vector2 View::WorldToScreen(const Vector2 &worldPosition)
 {
     return (worldPosition - position) * zoom + screenCenter;
 }
 
-Vector2 View::ScreenToWorld(const Vector2& screenPosition)
+Vector2 View::ScreenToWorld(const Vector2 &screenPosition)
 {
     return (screenPosition - screenCenter) / zoom + position;
 }
